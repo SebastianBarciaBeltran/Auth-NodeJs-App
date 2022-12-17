73e44d4d20c932dc76445ae1015c9eeda4054759
Sebastian Barcia Beltran <sebastianbarciabeltran@gmail.com>
2022-12-17 20:28:17 +0100

    cors and body configuration implemented

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 
 // Create the serve/app of express
 const app = express();
+
+// Cors
+app.use( cors() );
+
+// body
+app.use( express.json() );
 
 // Routes
 app.use( '/api/auth', require('./routes/auth') );
