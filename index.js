4681e88c2870f7c657d12885afcacb055082b4e0
SebastianBarciaBeltran <sebastianbarciabeltran@gmail.com>
2023-01-20 19:48:28 +0100

    remove /api/auth by /api

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use( cors() );
 app.use( express.json() );
 
 // Routes
-app.use( '/api/auth', require('./routes/auth.routes') );
+app.use( '/api', require('./routes/auth.routes') );
 
 app.listen( process.env.PORT, () => {
     console.log(`Server running in port ${ process.env.PORT } `)
