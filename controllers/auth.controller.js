82d816d400809adc9a7119843e34c9fbd70e77f3
SebastianBarciaBeltran <sebastianbarciabeltran@gmail.com>
2023-01-20 16:26:52 +0100

    add the steps to create new User

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,10 @@
-const { resp } = require('express');
+const { response } = require('express');
+const User = require('../models/User');
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const { generateJWT } = require('../helpers/jwt')
 
-const loginUser =  (request, response) => { 
+const loginUser =  (request, response = response) => { 
     
     const errors = validationResult(request);
 
@@ -20,28 +23,62 @@ const loginUser =  (request, response) => {
     })
 }
 
-const renewToken = (request, response) => { 
+const renewToken = (request, response = response) => { 
     return response.json({
         ok: true,
         msg: 'JWT'
     })
 }
 
-const createUser = (request, response = resp ) => { 
+const createUser = async(request, response = response ) => { 
     const errors = validationResult(request);
     const { name, email, password } = request.body;
 
-    if (!errors.isEmpty()) {
-        return response.status(400).json({
-            ok: false,
-            errors: errors.mapped()
+    try {
+        // Check if email exist 
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return response.status(400).json({
+                ok: false,
+                msg: 'User exist with this email'
+            });
+        }   
+        
+        // CREATE USER WITH MODEL 
+        const userDB = new User( request.body );
+
+        // Has the password
+        const salt = bcrypt.genSaltSync();
+        userDB.password = bcrypt.hashSync( password, salt );
+
+
+        // Generate JWT to send Angular
+        const token = await generateJWT(userDB.id ,name)
+
+        // Create User in DB 
+        await userDB.save();
+
+        // Generate the correct response 
+        return response.status(201).json({
+            ok: true,
+            uid: userDB.id,
+            name,
+            token
         });
+
+        
+    } catch (error) {
+        console.log('error: ', error);
+        return response.status(500).json({
+            ok: false,
+            msg: 'Please contact to the administration'
+        })
     }
 
-    return response.json({
-        ok: true,
-        msg: 'create user /new'
-    })
+
+
+
 } 
 
 
