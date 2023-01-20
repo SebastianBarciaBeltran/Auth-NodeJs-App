8b13d0c1bdeb2c66e94874294eeaf6f7143c39f5
SebastianBarciaBeltran <sebastianbarciabeltran@gmail.com>
2023-01-20 16:26:17 +0100

    create file jwt.js to generate the jwt

---
+++ helpers/jwt.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+
+const generateJWT = (uid, name) => {
+
+    const payload = { uid, name };
+
+    return new Promise( (resolve, reject) => {
+        jwt.sign(payload, process.env.SECRET_JWT_SEED, {
+            expiresIn: '12h'  
+        }, (error, token) => {
+    
+            if (error) {
+                console.log(error);
+                reject(error)
+            } else {
+                resolve(token);
+            }
+    
+        });
+
+    });
+
+
+}
+
+
+module.exports = {
+    generateJWT
+}
